eb2e7f231c740b831cfda0bcc728630c26ad9ed8
duongthinhdev <duongthinh12a4121105@gmail.com>
2026-03-06 11:29:25 +0700

    Model

---
+++ BackEnd/Model/BuoiHoc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace He_Thong_Diem_Danh_Qr.BackEnd.Model
+{
+    public class BuoiHoc
+    {
+        public int session_id { get; set; }
+
+        public string class_id { get; set; }
+
+        public DateTime ngay_hoc { get; set; }
+
+        public TimeSpan start_time { get; set; }
+
+        public TimeSpan end_time { get; set; }
+
+        public string qr_secret { get; set; }
+
+        public bool is_active { get; set; }
+    }
+}
---
+++ BackEnd/Model/SinhVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace He_Thong_Diem_Danh_Qr.BackEnd.Model
+{
+    public class SinhVien
+    {
+        public string msv { get; set; }
+
+        public string hoten { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
